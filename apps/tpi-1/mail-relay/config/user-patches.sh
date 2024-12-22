ae97b9612bab85ab9e1919dd44a2d3f38f07f61b
Waltteri Hooli <waltteri.hooli@gmail.com>
2024-12-23 00:16:37 +0200

    Add tpi 1 (#37)
    * Add tpi-1 cluster, update inventory
    
    * Fix kustomize paths for cloudflare-external-dns and cloudflare-tunnel. Disable liveness probe for mail-relay, since it's failing for some reason
    
    * Update tpi-1 apps, change nas and tpi-1 source branches

---
+++ apps/tpi-1/mail-relay/config/user-patches.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+rm -f /etc/postfix/{ldap-groups.cf,ldap-domains.cf,relayhost_map,sasl_passwd}
+cp /tmp/docker-mailserver/postfix-relaymap.cf /etc/postfix/relayhost_map
+cp /tmp/docker-mailserver/postfix-sasl-password.cf /etc/postfix/sasl_passwd
+chown root:root /etc/postfix/{relayhost_map,sasl_passwd}
+chmod 0600 /etc/postfix/{relayhost_map,sasl_passwd}
+
+postconf \
+    "smtp_sasl_password_maps = texthash:/etc/postfix/sasl_passwd" \
+    "sender_dependent_relayhost_maps = texthash:/etc/postfix/relayhost_map" \
+    "smtpd_sasl_auth_enable = yes" \
+    "smtp_sasl_auth_enable = yes" \
+    "smtp_sasl_security_options = noanonymous" \
+    "smtp_tls_security_level = encrypt" \
+    "smtp_sender_dependent_authentication = yes" \
+    "virtual_mailbox_domains = /etc/postfix/vhost" \
+    "virtual_alias_maps = texthash:/etc/postfix/virtual"
+
+sed -i /etc/postfix/ldap-senders.cf \
+    -e '/result_format/d' \
+    -e '/result_attribute/d'
+cat <<EOF >> /etc/postfix/ldap-senders.cf
+result_attribute = uid
+result_format = %s@default.svc.cluster.local
+EOF
+postfix reload
