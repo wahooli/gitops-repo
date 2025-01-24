50fa37b1ae645abe93ddca7dfaafcac38d5dd460
Waltteri Hooli <waltteri.hooli@gmail.com>
2025-01-24 13:34:55 +0200

    Modify pod view grafana dashboard, change mail-relay envs and add prometheus exporter.

--- apps/tpi-1/mail-relay/config/user-patches.sh
+++ apps/tpi-1/mail-relay/config/user-patches.sh
@@ -5,11 +5,13 @@ cp /tmp/docker-mailserver/postfix-sasl-password.cf /etc/postfix/sasl_passwd
 chown root:root /etc/postfix/{relayhost_map,sasl_passwd}
 chmod 0600 /etc/postfix/{relayhost_map,sasl_passwd}
 
+# default relayhost = DUNNO should bounce emails if relayhost isn't mapped to sender domain
 postconf \
     "smtp_sasl_password_maps = texthash:/etc/postfix/sasl_passwd" \
     "sender_dependent_relayhost_maps = texthash:/etc/postfix/relayhost_map" \
     "smtpd_sasl_auth_enable = yes" \
     "smtp_sasl_auth_enable = yes" \
+    "relayhost = DUNNO" \
     "smtp_sasl_security_options = noanonymous" \
     "smtp_tls_security_level = encrypt" \
     "smtp_sender_dependent_authentication = yes" \
