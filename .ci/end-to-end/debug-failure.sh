24bee13626de94f809870e6d41994866382ebb23
Waltteri Hooli <waltteri.hooli@gmail.com>
2025-07-24 01:02:19 +0300

    Migrate authentik to use patroni

--- .ci/end-to-end/debug-failure.sh
+++ .ci/end-to-end/debug-failure.sh
@@ -2,8 +2,8 @@
 HELM_RELEASES="${HELM_RELEASES:-$1}"
 DEBUG_STORAGE="${DEBUG_STORAGE:-false}"
 DEBUG_LOGS="${DEBUG_LOGS:-false}"
-DEBUG_LOGS_NAMESPACES="${DEBUG_LOGS_NAMESPACES:-default cert-manager internal-dns logging}"
-DESCRIBE_PODS_NAMESPACES="${DESCRIBE_PODS_NAMESPACES:-default cert-manager monitoring logging}"
+DEBUG_LOGS_NAMESPACES="${DEBUG_LOGS_NAMESPACES:-default cert-manager internal-dns logging authentik}"
+DESCRIBE_PODS_NAMESPACES="${DESCRIBE_PODS_NAMESPACES:-default cert-manager monitoring logging authentik}"
 
 echo "::group::Describe all cluster nodes"
 kubectl describe nodes -A
